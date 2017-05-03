0d5e96f84c6c3043c3d4f709ef0963cbe5c55864
UHanczar <u.hanczar@gmail.com>
2017-05-04 00:02:48 +0300

    Creating_Controls

---
+++ app/components/Controls.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Controls = React.createClass({
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+  onStatusChange(newStatus) {
+    // or onstead of return arrow function here we can write in render
+    // onClick={() => this.onStatusChange('stated')
+    return () => {
+      this.props.onStatusChange(newStatus);
+    };
+},
+  render() {
+    const {countdownStatus} = this.props;
+    const renderStartStopButton = () => {
+      if (countdownStatus === 'started') {
+        return <button  className='button secondary' onClick={this.onStatusChange('paused')}>Pause</button>
+      } else if (countdownStatus === 'paused') {
+        return <button className='button primary' onClick={this.onStatusChange('stated')}>Start</button>
+      }
+    };
+
+    return(
+      <div className='controls'>
+        {renderStartStopButton()}
+        <button className='button alert hollow' onClick={this.onStatusChange('stopped')}>Clear</button>
+      </div>
+    );
+  }
+});
+
+export default Controls;
